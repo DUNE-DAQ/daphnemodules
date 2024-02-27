db3e56a8a7839b197711b3ba61c7988cec4ae6e9
Marco Roda <marco.roda.88@gmail.com>
2024-02-27 10:28:37 +0000

    Remove attic

--- attic/clocks.cpp
+++
@@ -1,9 +0,0 @@
-#include "configclk.hpp"
-
-int main() {
-    for (int i : {4, 5, 7, 9, 11, 12, 13}) {
-        std::string ip = ("10.73.137." + std::to_string(i + 100));
-        config c((ip).c_str(),i);
-    }
-    return 0;
-}
