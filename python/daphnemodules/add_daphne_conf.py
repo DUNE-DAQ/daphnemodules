8a095fe47d5c3099eb4b34233da8a6df6554ba23
Marco Roda <mroda@liverpool.ac.uk>
2024-11-20 17:18:36 +0100

    script is working

--- python/daphnemodules/add_daphne_conf.py
+++ python/daphnemodules/add_daphne_conf.py
@@ -5,7 +5,7 @@ import os
 import json
 import sys
 
-def add_daphne_conf(oksfile, object_name, json_file, timeout_ms = 1500):
+def add_daphne_conf(oksfile:str, object_name:str, json_file:str, timeout_ms:int = 1500):
     """Script to add a new DaphneConf object from a given json file"""
 
     print( "Adding file", json_file, "to object", object_name)
@@ -15,14 +15,14 @@ def add_daphne_conf(oksfile, object_name, json_file, timeout_ms = 1500):
     with open(json_file, 'r') as file:
         data = json.load(file)
 
-#    schemafile=f'{os.environ["DAL_SHARE"]}/schema/appmodel/PDS.schema.xml'
-#    dal = conffwk.dal.module('dal', schemafile)
+    schemafile='schema/appmodel/PDS.schema.xml'
+    dal = conffwk.dal.module('dal', schemafile)
 
-#    new_conf = dal.DaphneConf(object_name,
-#                              timeout_ms=timeout_ms,
-#                              configuration_file=data)
+    new_conf = dal.DaphneConf(object_name,
+                              timeout_ms=timeout_ms,
+                              configuration_file=data)
 
-#    db.update_dal(new_conf)
-#    db.commit()
+    db.update_dal(new_conf)
+    db.commit()
 
 
