2251355743c2c98d0b7f6c2d14ed87b38f6f35e8
Marco Roda <mroda@liverpool.ac.uk>
2025-09-03 16:56:10 +0200

    Starting updating script to get rid of the json in the configuration

--- python/daphnemodules/add_daphne_conf.py
+++ python/daphnemodules/add_daphne_conf.py
@@ -5,6 +5,7 @@ import os
 import json
 import sys
 
+
 def add_daphne_conf(oksfile:str, object_name:str, json_file:str, timeout_ms:int = 1500):
     """Script to add a new DaphneConf object from a given json file"""
 
@@ -13,58 +14,79 @@ def add_daphne_conf(oksfile:str, object_name:str, json_file:str, timeout_ms:int 
     db = conffwk.Configuration("oksconflibs:" + oksfile)
 
     with open(json_file, 'r') as file:
-        data = file.read()
+        data = json.load(file)
 
-    schemafile='schema/appmodel/PDS.schema.xml'
-    dal = conffwk.dal.module('dal', schemafile)
+    print(data)
+
+    for key, value in data.items() :
+        slot = value["slot"]
+        channels = dict()
+        
+        raw_channels = value["channel_analog_conf"]
+        raw_ids = raw_channels["ids"]
+        raw_gains = raw_channels["gains"]
+        raw_offsets = raw_channels["offsets"]
+        raw_trims = raw_channels["trims"]
+        for i, idx in enumerate(raw_ids):   ## idx in [0-39]
+            c = channels[idx] = dal.DaphneV2Channel( f"{object_name}-daphne-{slot}-channel-{idx}",
+                                                     channel_id=idx,
+                                                     gain=raw_gains[i],
+                                                     offset=raw_offsets[i],
+                                                     trim=raw_trims[i] )
+            db.update_dal(c)
+            
+
+    
+##    schemafile='schema/appmodel/PDS.schema.xml'
+##    dal = conffwk.dal.module('dal', schemafile)
 
     ## create default objects, they will override old configurations
-    def_channel = dal.DaphneV2Channel( "daphne-v2-default-channel",
-                                       channel_id=100,
-                                       gain=1,
-                                       offset=2200,
-                                       trim=0 )
-    db.update_dal(def_channel)
+##    def_channel = dal.DaphneV2Channel( "daphne-v2-default-channel",
+##                                       channel_id=100,
+##                                       gain=1,
+##                                       offset=2200,
+##                                       trim=0 )
+##    db.update_dal(def_channel)
 
-    def_adc = dal.DaphneV2ADC( "daphne-v2-default-adc",
-                               low_resolution=False,
-                               output_offset_binary=True,
-                               MSB_first=True)
-    db.update_dal(def_adc)
+##    def_adc = dal.DaphneV2ADC( "daphne-v2-default-adc",
+##                               low_resolution=False,
+##                               output_offset_binary=True,
+##                               MSB_first=True)
+##    db.update_dal(def_adc)
     
-    def_pga = dal.DaphneV2PGA( "daphne-v2-default-pga",
-                               lpf_cut_frequency=4,
-                               integrator_disable=True,
-                               gain=False)
-    db.update_dal(def_pga)
+##    def_pga = dal.DaphneV2PGA( "daphne-v2-default-pga",
+##                              lpf_cut_frequency=4,
+##                               integrator_disable=True,
+##                               gain=False)
+##    db.update_dal(def_pga)
     
-    def_lna = dal.DaphneV2LNA( "daphne-v2-default-lna",
-                               clamp=0,
-                               integrator_disable=True,
-                               gain=2)
-    db.update_dal(def_lna)
+##    def_lna = dal.DaphneV2LNA( "daphne-v2-default-lna",
+ ##                              clamp=0,
+##                               integrator_disable=True,
+##                               gain=2)
+##    db.update_dal(def_lna)
     
-    def_afe = dal.DaphneV2AFE( "daphne-v2-default-afe",
-                               afe_id=100,
-                               attenuator=2666,
-                               v_bias=0,
-                               adc=def_adc,
-                               pga=def_pga,
-                               lna=def_lna)
-    db.update_dal(def_afe)
+##    def_afe = dal.DaphneV2AFE( "daphne-v2-default-afe",
+##                               afe_id=100,
+##                               attenuator=2666,
+##                               v_bias=0,
+##                               adc=def_adc,
+##                               pga=def_pga,
+##                               lna=def_lna)
+##    db.update_dal(def_afe)
 
-    def_board = dal.DaphneV2BoardConf( "daphne-v2-default-board",
-                                       bias_ctrl=0,
-                                       self_trigger_threshold=0,
-                                       default_channel=def_channel,
-                                       default_afe=def_afe )
-    db.update_dal(def_board)
+##    def_board = dal.DaphneV2BoardConf( "daphne-v2-default-board",
+##                                       bias_ctrl=0,
+##                                       self_trigger_threshold=0,
+##                                       default_channel=def_channel,
+##                                       default_afe=def_afe )
+##    db.update_dal(def_board)
     
-    new_conf = dal.DaphneConf(object_name,
-                              timeout_ms=timeout_ms,
-                              json_file=data,
-                              default_v2_settings=def_board )
-    db.update_dal(new_conf)
+##    new_conf = dal.DaphneConf(object_name,
+##                              timeout_ms=timeout_ms,
+##                              json_file=data,
+##                              default_v2_settings=def_board )
+##    db.update_dal(new_conf)
     
     db.commit()
 
