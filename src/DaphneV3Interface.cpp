ca0c88a4a2d5d66a90c43e22e6cfe9d146f844ce
Marco Roda <mroda@liverpool.ac.uk>
2025-09-24 12:35:42 +0200

    Adding an initial backbone for the interface

---
+++ src/DaphneV3Interface.cpp
@@ -0,0 +1,68 @@
+/** 
+ *  
+ * Implementations of DaphneV3Interface's functions                                                                    
+ * 
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.                                                      
+ * Licensing/copyright details are in the COPYING file that you should have         
+ */
+
+#include "DaphneV3Interface.hpp"
+#include "logging/Logging.hpp"
+
+#include <fmt/format.h>
+#include <regex>
+
+#warning CHECK IF WE NEED THIS
+#include <sys/time.h>
+
+using namespace dunedaq::daphnemodules;
+
+DaphneV3Interface::DaphneV3Interface( std::string address,
+				      std::chrono::milliseconds timeout)
+  : m_context(1)
+  , m_socket(m_context, zmq::socket_type::req)
+  , m_timeout(timeout) {
+
+  
+  // find out if the address has a port with a regex
+  static const std::regex ip_with_port("^[^/\s:]+(?::\d{1,5})?$");
+  std::smatch string_values; 
+  if (! std::regex_match( address, string_values, ip_with_port ) ) {
+    // throw that the address is wrong
+  }
+
+  auto connection = string_values.size() > 1 ?
+    fmt::format("tcp://{}", address) :
+    fmt::format("tcp://{}:{}", address, s_default_control_port) ;
+  m_socket.connect(connection);
+
+  #warning RE-INSTATE
+  // if ( ! validate_connection() )
+    //throw FailedPing(ERS_HERE, ipaddr, port );
+
+}
+
+
+void DaphneV3Interface::close() {
+
+  const std::lock_guard<std::mutex> lock(m_access_mutex);
+  m_socket.set(zmq::sockopt::linger, 0);
+  m_socket.close();
+  
+}
+
+
+bool DaphneV3Interface::validate_connection() const {
+
+  static const uint64_t good_value = 0xdeadbeef; 
+
+  #warning FIX ME
+  return true;
+}
+
+
+
+
+
+
+
---
+++ src/DaphneV3Interface.hpp
@@ -0,0 +1,106 @@
+/**
+ * @file DaphneV2Interface.hpp
+ *
+ * Definition of the interface protocol to the daphne V3 boards
+ *  It's basically just a wrapper around a zmq socket
+ *
+ * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ *
+ */
+
+#ifndef DAPHNEMODULES_SRC_DAPHNEV3INTERFACE_HPP_
+#define DAPHNEMODULES_SRC_DAPHNEV3INTERFACE_HPP_ 
+
+#include <memory>
+#include <mutex>
+#include <functional>
+
+
+#include <ers/ers.hpp>
+#include "logging/Logging.hpp" // NOTE: if ISSUES ARE DECLARED BEFORE include logging/Logging.hpp, TLOG_DEBUG<<issue wont work.
+
+#include <zmq.hpp>
+
+
+namespace dunedaq {
+
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     SocketCreationError,
+		     "Failed to create a socket",
+		     ERS_EMPTY
+		   ) 
+
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     InvalidAddress,
+		     "Invalid address: " << address,
+		     ((std::string)address)
+		   ) 
+  
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     FailedPing,
+		     "Failed to ping daphne board at " << ip << ':' << port,
+		     ((std::string)ip)((int)port)
+		   ) 
+
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     FailedSocketInteraction,
+		     "Failed to call " << command,
+		     ((std::string)command)
+		   ) 
+
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     CommandTimeout,
+		     "Command " << command << " timed out after " << timeout_us << " microseconds",
+		     ((std::string)command)((unsigned int)timeout_us)
+		   ) 
+
+  ERS_DECLARE_ISSUE( daphnemodules,
+		     SocketTimeout,
+		     "Socket timed out after " << timeout_us << " microseconds",
+		     ((unsigned int)timeout_us)
+		   ) 
+ 
+  } // dunedaq namespace
+
+
+namespace dunedaq::daphnemodules {
+
+  class DaphneV3Interface {
+
+  public:
+    DaphneV3Interface( std::string address,  // it can contain the port or not
+		       std::chrono::milliseconds timeout = std::chrono::milliseconds(500));
+    
+
+    ~DaphneV3Interface() { close(); }
+
+    DaphneV3Interface(const DaphneV3Interface &) = delete;
+    DaphneV3Interface & operator= (const DaphneV3Interface & ) = delete;
+    DaphneV3Interface(DaphneV3Interface &&) = delete;
+    DaphneV3Interface & operator= (DaphneV3Interface &&) = delete;
+
+    bool validate_connection() const ;
+
+    
+  protected:
+    
+    void close();
+    
+  private:
+    zmq::context_t m_context;
+    
+    zmq::socket_t m_socket; 
+    mutable std::mutex m_access_mutex;
+    
+    std::chrono::milliseconds m_timeout{1000};
+
+    static const size_t s_default_control_port = 40001;
+  }; 
+  
+
+} // namespce  dunedaq::daphnemodules
+
+
+#endif // DAPHNEMODULES_SRC_DAPHNEV3INTERFACE_HPP_
